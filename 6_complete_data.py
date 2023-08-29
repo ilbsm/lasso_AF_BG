add25d0d071e0dd9dac9566e95976a70912d5ef5
baaagr <bartoszgren@gmail.com>
2023-08-29 19:39:04 +0200

    Ranking

--- 6_complete_data.py
+++ 6_complete_data.py
@@ -19,30 +19,39 @@ def read_cif(cif_file):
             ndx2plddt[int(ndx)] = float(plddt)
     return ndx2plddt, fasta
 
-def get_plddt(ndx2plddt, bridge, lasso):
+def get_plddt(ndx2plddt, bridge, lasso, edges):
     plddt_global = np.mean(list(ndx2plddt.values()))
     plddt_global_min = min(list(ndx2plddt.values()))
     if bridge != '-':
-        beg, end = [int(x) for x in bridge.split('-')]
-        plddt_cys1 = ndx2plddt[beg]
-        plddt_cys2 = ndx2plddt[end]
-        if lasso[-1] == 'N':
-            plddt_short = [v for k,v in ndx2plddt.items() if k <= end]
-            plddt_local = np.mean(plddt_short)
-            plddt_local_min = min(plddt_short)
-        elif lasso[-1] == 'C':
-            plddt_short = [v for k,v in ndx2plddt.items() if k >= beg]
-            plddt_local = np.mean(plddt_short)
-            plddt_local_min = min(plddt_short)
-        else:
-            plddt_local = plddt_global
-            plddt_local_min = plddt_global_min
+        cys1, cys2 = [int(x) for x in bridge.split('-')]
+        beg, end = edges
+        plddt_cys1 = ndx2plddt[cys1]
+        plddt_cys2 = ndx2plddt[cys2]
+        plddt_short = [v for k,v in ndx2plddt.items() if beg <= k <= end]
+        plddt_local = np.mean(plddt_short)
+        plddt_local_min = min(plddt_short)
     else:
         plddt_local = -1
         plddt_local_min = -1
         plddt_cys1 = -1
         plddt_cys2 = -1
     return plddt_cys1, plddt_cys2, plddt_local, plddt_local_min, plddt_global, plddt_global_min
+
+def find_lasso_edges():
+    uniid2edges = {}
+    with open('lassos_surfaces_no_dupl.tsv', 'r') as f:
+        for line in f.readlines():
+            record_id, lasso, _, _, Ncross, Ccross, *_ = line.strip().split()
+            uniprot_id, bridge = record_id.split('_')
+            uniprot_id = uniprot_id.split('-')[1]
+            ndxes = set([int(x) for x in bridge.split('-')])
+            if Ncross != '""':
+                ndxes |= set([int(x.strip('*+-')) for x in Ncross.split(',')])
+            if Ccross != '""':
+                ndxes |= set([int(x.strip('*+-')) for x in Ccross.split(',')])
+            uniid2edges[uniprot_id] = min(ndxes), max(ndxes)
+    return uniid2edges
+
 
 def prepare():
     clusters = set([])
@@ -53,10 +62,11 @@ def prepare():
     clusters = sorted(clusters)
     with open('failed_downloads.txt', 'r') as f:
         absent = set(f.read().split('\n'))
-    return clusters, absent
+    uniid2edges = find_lasso_edges()
+    return clusters, absent, uniid2edges
 
 if __name__ == '__main__':
-    clusters, absent = prepare()
+    clusters, absent, uniid2edges = prepare()
     for cluster in tqdm(clusters, total = len(clusters)):
         with open('clusters/{}.txt'.format(cluster), 'r') as f:
             with open('clusters2/{}.txt'.format(cluster), 'w') as g:
@@ -69,14 +79,20 @@ if __name__ == '__main__':
                         if uniprot_id in absent:
                             lasso = 'no_struct'
                             plddts_str = '   '.join(['-1']*6)
+                            edges = bridge
                         else:
                             ndx2plddt, fasta = read_cif('cifs/AF-{}-F1-model_v4.cif'.format(uniprot_id))
-                            plddts = get_plddt(ndx2plddt, bridge, lasso)
+                            if uniprot_id in uniid2edges:
+                                edges = uniid2edges[uniprot_id]
+                            else:
+                                edges = bridge
+                            plddts = get_plddt(ndx2plddt, bridge, lasso, edges)
                             plddts = [str(round(x,1)) for x in plddts]
                             plddts_str = '{:4} {:4} {:4} {:4} {:4} {:4}'.format(*plddts)
                             gg.write('>{}\n'.format(uniprot_id))
                             gg.write(fasta + '\n')
-                        record = [uniprot_id, lasso, bridge, plddts_str]
+                        edges = '-'.join([str(x) for x in edges])
+                        record = [uniprot_id, lasso, bridge, edges, plddts_str]
                         if lasso[0] == 'L':
                             to_write.append(record)
                         elif lasso[0] == 'n':
@@ -86,7 +102,7 @@ if __name__ == '__main__':
                     to_write += to_write_else
                     to_write += to_write_no_struct
                     for record in to_write:
-                        g.write('{:10} {:28} {:7} {}\n'.format(*record))
+                        g.write('{:10} {:28} {:7} {:7} {}\n'.format(*record))
 
 
 
--- 7_plddt_ranking.py
+++ 7_plddt_ranking.py
@@ -1,7 +1,9 @@
 import numpy as np
 import os
 
-records = []
+records70 = []
+records80 = []
+records90 = []
 infiles = sorted(os.listdir('clusters2'))
 for infile in infiles:
     if infile[0] == '.':
@@ -10,28 +12,48 @@ for infile in infiles:
         name = '{:10}'.format(infile.split('.')[0].split('_')[1])
         c1 = []
         c2 = []
-        mean_plddt_local = []
-        min_plddt_local = []
+        mean_local = []
+        min_local = []
         lassos = set([])
+        uniprot_ids = []
         for line in f.readlines():
-            uniprot_id, lasso, bridge, *plddts = line.strip().split()
+            uniprot_id, lasso, bridge, edge, *plddts = line.strip().split()
             if bridge != '-':
                 plddts = [float(x) for x in plddts]
                 c1.append(plddts[0])
                 c2.append(plddts[1])
-                mean_plddt_local.append(plddts[2])
-                min_plddt_local.append(plddts[3])
+                mean_local.append(plddts[2])
+                min_local.append(plddts[3])
                 lassos.add(lasso)
-        c1 = min(c1)
-        c2 = min(c2)
-        mean_plddt_local = round(np.mean(mean_plddt_local),1)
-        min_plddt_local = min(min_plddt_local)
+                uniprot_ids.append(uniprot_id)
+        c1 = min(c1), max(c1)
+        c2 = min(c2), max(c2)
+        mean_local = round(min(mean_local),1), round(max(mean_local),1)
+        min_local = min(min_local), max(min_local)
         lassos = ';'.join(sorted(lassos))
-        score = int(round(c1+c2+min_plddt_local, 0))
-        record = [score, name, c1, c2, mean_plddt_local, min_plddt_local, lassos]
-        records.append(record)
+        uniprot_ids = ';'.join(uniprot_ids)
+        for_scoring = [c1[1], c2[1], mean_local[0]]
+        score = int(round(sum(for_scoring), 0))
+        c1_str = '-'.join(map(str, c1))
+        c2_str = '-'.join(map(str, c2))
+        mean_local_str = '-'.join(map(str, mean_local))
+        min_local_str = '-'.join(map(str, min_local))
+        record = [score, name, c1_str, c2_str, mean_local_str, min_local_str, lassos, uniprot_ids]
+        if all([x>70 for x in for_scoring]):
+            if all([x>80 for x in for_scoring]):
+                if all([x>90 for x in for_scoring]):
+                    records90.append(record)
+                else:
+                    records80.append(record)
+            else:
+                records70.append(record)
 
-records.sort(reverse=True)
+records70.sort(reverse=True)
+records80.sort(reverse=True)
+records90.sort(reverse=True)
 with open('plddt_ranking.txt', 'w') as f:
-    for record in records:
-        f.write(' '.join([str(x) for x in record]) + '\n')
+    f.write('score protein_id c1 c2 avg min lassos\n')
+    for records in [records90, records80, records70]:
+        f.write('=====================================\n')
+        for record in records:
+            f.write(' '.join([str(x) for x in record]) + '\n')
